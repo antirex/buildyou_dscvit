d91fe64535924d021edd3a5bf0e1c4260537cb31
antirex <anshulbamb22@gmail.com>
2020-08-24 12:08:26 +0530

    carousel+fixes

---
+++ promotional website/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+    slidesToShow: 3,
+    infinite:true,
+    slidesToScroll: 1,
+    cssEase: 'linear',
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    });
+  });
+ 
